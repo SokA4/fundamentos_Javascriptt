551cd7e8b4fa7d0f39caa0be6e511affa292b1e8
SokA4 <Luisecheverria@liceovvh.cl>
2025-06-02 09:23:37 -0400

    avance

--- 02_tipos_datos-onclick/static/js/script.js
+++ 02_tipos_datos-onclick/static/js/script.js
@@ -1,24 +1,26 @@
-let numeroEntero = 25;
-let numeroDecimal = 3.14;
-let cadenaTexto = "Hola, mundo";
+let diasDelMes = 31;
+let cuartaPartede10 = 2.5;
+let Saludo = "Que onda!!";
 let valorBooleano = true;
 let arregloNumeros = [1, 2, 3, 4];
 let objetoPersona = { nombre: "Ana", edad: 30 };
 let valorIndefinido;
 let valorNulo = null;
 const mes = "Junio";
+let arregloColores = ["verde", " morado", " rojo"];
+let objetoUsuario = { nombre: "soka", correo: "luisecheverria@liceovvh.cl", edad: 30 };
 
 // Funciones para mostrar cada valor con alert()
-function mostrarNumeroEntero() {
-  alert("Número entero: " + numeroEntero);
+function mostrarDiasdelmes() {
+  alert("Dias del mes " + diasDelMes);
 }
 
 function mostrarNumeroDecimal() {
-  alert("Número decimal: " + numeroDecimal);
+  alert("Cuarta parte de 10: " + cuartaPartede10);
 }
 
 function mostrarCadenaTexto() {
-  alert("Cadena de texto: " + cadenaTexto);
+  alert("Saludo: " + Saludo);
 }
 
 function mostrarBooleano() {
@@ -43,4 +45,12 @@ function mostrarNulo() {
 
 function mostrarConstante() {
   alert("Constante mes: " + mes);
+}
+
+function mostrarColores() {
+  alert("Arreglo de colores: " + arregloColores);
+}
+
+function mostrarUsuario() {
+  alert("Objeto usuario: " + JSON.stringify(objetoUsuario));
 }
