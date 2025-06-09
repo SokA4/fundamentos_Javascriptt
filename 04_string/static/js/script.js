928286d7815546057ac2461d5752f2c5e86ca820
SokA4 <Luisecheverria@liceovvh.cl>
2025-06-09 08:48:15 -0400

    Update script.js

---
+++ 04_string/static/js/script.js
@@ -0,0 +1,8 @@
+//Uso de String Ejemplo:
+
+let nombre ="Luis" //variable tipo texto
+
+function usarString() {
+    let primeraLetra = nombre[0];
+    alert ("primera letra es: " + primeraLetra)
+}
