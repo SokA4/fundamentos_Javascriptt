1fa22bab6bbcb76740db12cf3375af16c5dfb122
SokA4 <Luisecheverria@liceovvh.cl>
2025-06-11 10:34:28 -0400

    Update script.js

--- 04_string/static/js/script.js
+++ 04_string/static/js/script.js
@@ -1,8 +1,13 @@
 //Uso de String Ejemplo:
 
-let nombre ="Luis" //variable tipo texto
+let nombre ="Luis"; //variable tipo texto
 
 function usarString() {
     let primeraLetra = nombre[0];
-    alert ("primera letra es: " + primeraLetra)
+    let ultimaLetra = nombre[nombre.length -1] //Asignamos valor a primera letra con posicion 0 de nombre
+    alert ("primera letra es: " + primeraLetra +
+        "\nÜltima letra es:" + ultimaLetra
+    )
 }
+
+//tarea 
